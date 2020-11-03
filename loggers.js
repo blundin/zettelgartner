a55dc46f0b414b430d25a79023a642190f15a30c
Brian Lundin <brian.lundin@gmail.com>
2020-11-03 13:01:49 -0600

    console logging now supports -d and -v

--- loggers.js
+++ loggers.js
@@ -1,5 +1,32 @@
 const winston = require("winston");
+const defaultLogLevel = "info";
 
-const console = new winston.transports.Console({ level: "info" });
+const defaultConsole = winston.createLogger({
+  level: defaultLogLevel,
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console()
+  ]
+});
 
-exports.console = console;
+const configureConsoleLogger = function(options) {
+  var logLevel = defaultLogLevel;
+
+  if (options.debug == true) {
+    logLevel = "debug"
+  } else if (options.verbose == true) {
+    logLevel = "verbose"
+  }
+
+  const console = winston.createLogger({
+    level: logLevel,
+    format: winston.format.simple(),
+    transports: [
+      new winston.transports.Console()
+    ]
+  });
+  return console;
+};
+
+exports.defaultConsole = defaultConsole;
+exports.configureConsoleLogger = configureConsoleLogger;
--- main.js
+++ main.js
@@ -1,14 +1,47 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const loggers = require("./loggers.js")
 const errors = require("./errors.js");
 
 const args = process.argv.slice(2);
 
+var directoryPath = "";
+var debug = false;
+var verbose = false;
+var help = false;
+
 if (args.length > 0 && args[0] != undefined) {
   for (i = 0; i < args.length; i++) {
-    console.log(args[i]);
+    if (args[i] == "-d" || args[i] == "--debug") {
+      debug = true;
+      console.info("debug = true");
+    } else if (args[i] == "-v" || args[i] == "--verbose") {
+      verbose = true;
+      console.info("verbose = true");
+    } else if (args[i] == "-h" || args[i] == "--help") {
+      help = true;
+      console.info("help = true");
+    } else {
+      if (directoryPath == "") {
+
+      }
+    }
+  }
+
+  if (help == true) {
+    // print out help info
+  } else {
+    const options = {
+        directoryPath: directoryPath,
+        debug: debug,
+        verbose: verbose
+    };
+
+    const console = loggers.configureConsoleLogger(options);
+    console.debug("debug logging is on.");
+    console.verbose("verbose logging is on.");
   }
 } else {
-  console.log("ERROR: " + errors.INVALID_DIRECTORY_PATH);
+  const console = loggers.console;
+  console.error(errors.INVALID_DIRECTORY_PATH);
   process.exitCode = 1;
 }
