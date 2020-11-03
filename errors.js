fded775507c2a3ea74caad63492d90f3b64f8f94
Brian Lundin <brian.lundin@gmail.com>
2020-11-03 10:12:35 -0600

    basic error handling pattern implemented.

---
+++ errors.js
@@ -0,0 +1,5 @@
+const INVALID_DIRECTORY_PATH = "You must provide a valid directory path as an argument.";
+
+module.exports = {
+  INVALID_DIRECTORY_PATH
+}
--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-const fs = require("fs")
+// Modules
+const fs = require("fs");
 
+// Configuration
+const errors = require("./errors.js");
 const args = process.argv.slice(2);
 
-if (args.length < 1) {
-
+if (args.length >= 1) {
+  console.log(args[0]);
 } else {
-
+  if (args.length == 0) {
+    console.log(errors.INVALID_DIRECTORY_PATH);
+    process.exitCode = 1;
+  }
 }
