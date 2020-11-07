cb4fd96486a97f105ef700038f691b19d9968723
Brian Lundin <brian.lundin@gmail.com>
2020-11-06 23:47:03 -0600

    fixed options tests + started on file reads

--- main.js
+++ main.js
@@ -1,37 +1,40 @@
-'use strict'
-
 const options = require('./lib/options.js');
 const help = require('./lib/help.js');
-const loggers = require('./utils/loggers.js');
-const errors = require('./utils/errors.js');
-const Processor = require('./lib/NoteProcessor');
+const loggers = require('./lib/utils/loggers.js');
+const errors = require('./lib/utils/errors.js');
+const { getNoteFiles } = require('./lib/getNoteFiles');
 
 const args = process.argv.slice(2);
 
 if (args.length > 0) {
   const selectedOptions = options.parse(args);
-  const logger = loggers.configureConsoleLogger(selectedOptions);
+  const log = loggers.configureConsoleLogger(selectedOptions);
 
   if (!selectedOptions.help) {
     if (!selectedOptions.invalidPath) {
       if (!selectedOptions.invalidOption) {
-        logger.info('Started processing notes in ' + selectedOptions.directoryPath);
-        const processor = new Processor(selectedOptions.directoryPath);
+        log.info('Started processing notes in ' + selectedOptions.directoryPath);
 
+        getNoteFiles(selectedOptions.directoryPath, log)
+          .then((fileNames) => {
+            for (var i = 0; i < fileNames.length; i++) {
+
+            }
+          });
       } else {
-        logger.error(errors.INVALID_OPTION);
+        log.error(errors.INVALID_OPTION);
         help.printHelp();
         process.exitCode = 1;
-      } 
+      }
     } else {
-      logger.error(errors.INVALID_DIRECTORY_PATH);
+      log.error(errors.INVALID_DIRECTORY_PATH);
       help.printHelp();
       process.exitCode = 1;
     }
   }
 } else {
-  const logger = loggers.defaultConsole;
-  logger.error(errors.INVALID_DIRECTORY_PATH);
+  const log = loggers.defaultLogger;
+  log.error(errors.INVALID_DIRECTORY_PATH);
   help.printHelp();
   process.exitCode = 1;
 }
