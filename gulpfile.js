3487b4e94bbb3e316add515c4a442c10e1aed908
Brian Lundin <brian.lundin@gmail.com>
2020-11-08 19:23:47 -0600

    updating code style stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,26 @@
-const gulp = require('gulp');
-const childProcess = require('child_process');
-var clean = require('gulp-clean');
+const gulp = require("gulp");
+const childProcess = require("child_process");
+var clean = require("gulp-clean");
 
-const testFileDirectory = './test/test_data/';
-const cleanTestFileDirectory = './test/clean_test_data/';
+const testFileDirectory = "./test/test_data/";
+const cleanTestFileDirectory = "./test/clean_test_data/";
 
 function cleanTestFiles() {
-  return gulp.src(testFileDirectory + '*.*')
+  return gulp.src(testFileDirectory + "*.*")
     .pipe(clean());
 }
 
 function copyTestFiles() {
-  return gulp.src(cleanTestFileDirectory + '*.*')
+  return gulp.src(cleanTestFileDirectory + "*.*")
     .pipe(gulp.dest(testFileDirectory));
 }
 
 function run() {
-  return childProcess.exec('node main.js ' + testFileDirectory);
+  return childProcess.exec("node main.js " + testFileDirectory);
 }
 
 function test() {
-  return childProcess.exec('yarn test');
+  return childProcess.exec("yarn test");
 }
 
 exports.default = gulp.series(cleanTestFiles, copyTestFiles, run);
--- main.js
+++ main.js
@@ -1,14 +1,8 @@
-const fs = require('fs');
-const path = require('path');
-const unified = require('unified');
-const markdown = require('remark-parse');
-const { wikiLinkPlugin } = require('remark-wiki-link');
-
-const optionsParser = require('./lib/options.js');
-const help = require('./lib/help.js');
-const loggers = require('./lib/utils/loggers.js');
-const errors = require('./lib/utils/errors.js');
-const NoteFiles = require('./lib/notefiles.js');
+const optionsParser = require("./lib/options.js");
+const help = require("./lib/help.js");
+const loggers = require("./lib/utils/loggers.js");
+const errors = require("./lib/utils/errors.js");
+const NoteFiles = require("./lib/notefiles.js");
 
 const args = process.argv.slice(2);
 
@@ -16,29 +10,16 @@ if (args.length > 0) {
   const options = optionsParser.parse(args);
   const log = loggers.configureConsoleLogger(options);
 
+
   if (!options.help && !options.error) {
-    log.info('Started processing notes in ' + options.directoryPath);
+    log.info("Started processing notes in " + options.directoryPath);
 
-    NoteFiles.getFilenames(options.directoryPath, log)
+    NoteFiles.parseFiles(options.directoryPath, log)
       .then((fileNames) => {
-        log.debug('File names read: ' + fileNames);
 
-        const processor = unified()
-          .use(markdown, { gfm: true })
-          .use(wikiLinkPlugin);
-
-        var notes = [];
-        for (var i = 0; i < fileNames.length; i++) {
-          const filePath = path.join(options.directoryPath + fileNames[i]);
-          fs.readFile(filePath, 'utf8' , (err, data) => {
-            if (err) {
-              log.error(err);
-            }
-            var node = processor.parse(data)
-            log.debug(JSON.stringify(node, null, 2));
-            notes.push(node);
-          });
-        }
+      })
+      .catch((error) => {
+        log.error(error);
       });
   } else {
     if (options.error) {
